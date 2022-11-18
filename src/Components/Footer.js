04e0ed12941f98254230d25279e53145b0f00de1
Macanijus <tmacan975@gmail.com>
2022-11-18 11:48:16 +0100

    fonts

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,24 +4,39 @@ import "./footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <h5>Get the latest news</h5>
-      <input
-        className="footer-input"
-        type="text"
-        placeholder="your e-mail adress"
-      />
-      <button className="footer-button">
-        <i class="fa fa-paper-plane"></i>
-      </button>
+      <div className="latestNews">
+        <h5>Get the latest news</h5>
+        <input
+          className="footer-input"
+          type="text"
+          placeholder="your e-mail adress"
+        />
+        <button className="footer-button">
+          <i class="fa fa-paper-plane"></i>
+        </button>
+      </div>
 
-      <p className="copyright">© Hacker News 2022</p>
+      <div className="copyright">
+        <p>© Hacker News 2022</p>
+      </div>
 
       <div className="social-icons">
         <h5>Connect with us</h5>
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-linkedin"></i>
+        <div className="icons">
+          <a href="https://de-de.facebook.com/ " target="_blank">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://twitter.com/i/flow/login" target="_blank">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com/" target="_blank">
+            {" "}
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
